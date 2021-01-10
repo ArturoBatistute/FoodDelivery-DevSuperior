6e24c8f8b40d3332d63f44018edddf151587fd64
Arturo H Batistute <arturo.batistute@gmail.com>
2021-01-10 17:51:37 -0300

    Added: Search Order Location

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes'
-import Navbar from './Navbar';
 
 function App() {
   return (
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
 
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
+import StepsHeader from './StepsHeader';
+import { OrderLocationData, Product } from './types';
+import OrderLocation from './OrderLocation';
+
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+
+    useEffect(() => {
+        fetchProducts()
+        .then(res => setProducts(res.data))
+        .catch(err => console.log(err));
+    }, []);
 
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 }
