060dbd30da3bae127dede3985f21733eb137deec
Arturo H Batistute <arturo.batistute@gmail.com>
2021-01-09 23:41:00 -0300

    Added: Home Page

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
     return (
         <nav className="main-navbar">
         <Logo />
-        <a href="home" className="logo-text">DS Delivery</a>
+        <a href="home" className="logo-text">DS Food Delivery</a>
         </nav>
     )
 }
